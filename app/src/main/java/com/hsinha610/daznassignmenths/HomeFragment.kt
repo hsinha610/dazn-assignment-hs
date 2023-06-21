f5b0556e24c480337aeccad4b569d960e12a81c8
hsinha610 <hsinha610@gmail.com>
2023-06-22 02:07:12 +0530

    Added: HomeFragment

---
+++ app/src/main/java/com/hsinha610/daznassignmenths/HomeFragment.kt
@@ -0,0 +1,46 @@
+package com.hsinha610.daznassignmenths
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.fragment.app.Fragment
+import com.hsinha610.daznassignmenths.ui.FragmentComposeView
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return context?.let { context ->
+            FragmentComposeView(context) {
+                UI()
+            }
+        }
+    }
+
+    @Composable
+    fun UI() {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = "Hello",
+                modifier = Modifier.align(Alignment.Center),
+                color = MaterialTheme.colors.onPrimary
+            )
+        }
+    }
+
+
+}
--- app/src/main/java/com/hsinha610/daznassignmenths/MainActivity.kt
+++ app/src/main/java/com/hsinha610/daznassignmenths/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.hsinha610.daznassignmenths
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hsinha610.daznassignmenths.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initHomeFragment()
+    }
+
+    private fun initHomeFragment() {
+        val homeFragment = HomeFragment.newInstance()
+        supportFragmentManager.beginTransaction().apply {
+            replace(binding.fragmentContainerViewTag.id, homeFragment)
+            commit()
+        }
     }
 }
