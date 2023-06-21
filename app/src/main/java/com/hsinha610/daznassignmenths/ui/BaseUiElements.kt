408fb41370cd50094f546e9601f0ea1a907013a6
hsinha610 <hsinha610@gmail.com>
2023-06-22 04:33:52 +0530

    Added: Loading & Error Screen

--- app/src/main/java/com/hsinha610/daznassignmenths/ui/BaseUiElements.kt
+++ app/src/main/java/com/hsinha610/daznassignmenths/ui/BaseUiElements.kt
@@ -1,7 +1,13 @@
 package com.hsinha610.daznassignmenths.ui
 
 import android.content.Context
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 
@@ -14,5 +20,19 @@ fun FragmentComposeView(context: Context, Ui: @Composable () -> Unit): ComposeVi
                 Ui()
             }
         }
+    }
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+    }
+}
+
+@Composable
+fun ErrorScreen(error: String) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(error, modifier = Modifier.align(Alignment.Center))
     }
 }
